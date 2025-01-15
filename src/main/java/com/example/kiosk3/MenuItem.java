852ed6925377dc317cc0fe09ccd2b46d5e5e7f2d
lamgak12 <tntnqk12@naver.com>
2025-01-15 22:24:20 +0900

    feat(kiosk3/MenuItem): level3 메뉴아이템 기능 추가

---
+++ src/main/java/com/example/kiosk3/MenuItem.java
@@ -0,0 +1,31 @@
+package com.example.kiosk3;
+
+public class MenuItem {
+    private final String name;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | W %-2.1f | %s%n",getName(),getPrice(),getDescription());
+        //return "메뉴 이름: " + getName() + "  메뉴 가격: W " + getPrice() + "\t메뉴 설명: " + getDescription();
+    }
+}
