bb8284c5ef6eeb809b7f7ee2010e6c992a1a0260
lamgak12 <tntnqk12@naver.com>
2025-01-17 14:01:38 +0900

    refactor(Main): 요구사항에 맞게 로직변경

--- src/main/java/com/example/kiosk2/Main.java
+++ src/main/java/com/example/kiosk2/Main.java
@@ -1,32 +1,47 @@
 package com.example.kiosk2;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        MenuBoard menuBoard = new MenuBoard();
+        // 메뉴아이템 데이터 세팅
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         Scanner sc = new Scanner(System.in);
 
         while(true){
-            menuBoard.printMenu();
+            // 메뉴아이템 순차 출력
+            for (int i = 0; i < menuItems.size(); i++) {
+                System.out.printf("%d. "+menuItems.get(i)+"\n",(i+1));
+            }
+            try {
+                System.out.print("메뉴를 선택하세요: ");
+                int index = sc.nextInt();
+                sc.nextLine();
 
-            System.out.print("메뉴를 선택하세요: ");
-            int index = sc.nextInt();
-            sc.nextLine();
+                if (index == 0) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
 
-            if(index==0){
-                System.out.println("프로그램을 종료합니다.");
-                break;
+                System.out.println("선택한 메뉴: " + menuItems.get(index - 1));
+
+            }catch (InputMismatchException e){
+                // menuItems의 인덱스를 입력할때 숫자로 입력했는지 확인
+                System.out.println("숫자를 입력해주세요.");
+                sc.nextLine();
+            }catch (IndexOutOfBoundsException e){ // menuItems의 범위를 벗어나는지 확인
+                System.out.println("메뉴판에 없는 번호입니다.");
             }
 
-            try { // 선택된 아이템을 출력
-                MenuItem selectedItem = menuBoard.getItem(index);
-                System.out.println("선택한 메뉴: " + selectedItem);
-                break;
 
-            }catch (IndexOutOfBoundsException e){ // 예외 처리
-                System.out.println(e.getMessage());
-            }
         }
         sc.close();
     }
